ac63a4c1615596c67985adfd4aa57570f4abb189
Fang <me@fangqk.com>
2022-11-20 17:27:30 +0800

    (web-specs): v0.20.0 + 账号注册通知

--- src/web-specs/SignupSpecs.ts
+++ src/web-specs/SignupSpecs.ts
@@ -4,6 +4,7 @@ import { LoginService } from '../services/web/LoginService'
 import { SsoServer } from '../SsoServer'
 import assert from '@fangcha/assert'
 import { _FangchaState } from '@fangcha/backend-kit'
+import { SsoSession } from '../services/web/SsoSession'
 
 const factory = new SpecFactory('注册')
 
@@ -12,6 +13,9 @@ factory.prepare(SignupApis.SimpleSignup, async (ctx) => {
   const ssoServer = ctx.ssoServer as SsoServer
   const accountV2 = await ssoServer.accountServer.createAccount(ctx.request.body)
   await new LoginService(ctx).onLoginSuccess(accountV2)
+  const session = ctx.session as SsoSession
+  const email = session.getAuthInfo().email
+  _FangchaState.botProxy.notify(`${email} 在 ${session.getRefererUrl()} 注册了账号.`)
   ctx.status = 200
 })
 
