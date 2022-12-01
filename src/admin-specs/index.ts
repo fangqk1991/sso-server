459e0abd54e424c684174bcd1cec4ab0befbbff0
Fang <me@fangqk.com>
2022-12-01 13:09:07 +0800

    v0.21.0: remove admin-sdk

--- src/admin-specs/index.ts
+++
@@ -1,8 +0,0 @@
-import { SwaggerDocItem } from '@fangcha/router'
-import { SsoClientSpecs } from './SsoClientSpecs'
-
-export const SsoClientDocItem: SwaggerDocItem = {
-  name: 'SSO Admin',
-  pageURL: '/api-docs/v1/sso-admin',
-  specs: SsoClientSpecs,
-}
